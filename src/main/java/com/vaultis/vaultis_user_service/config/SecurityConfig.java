580217adaeeef3fef2143f8a7d2933c6c456af39
janghs <gorae930301@gmail.com>
2025-01-03 17:30:41 +0900

    페이지 구성

--- src/main/java/com/vaultis/vaultis_user_service/config/SecurityConfig.java
+++ src/main/java/com/vaultis/vaultis_user_service/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.vaultis.vaultis_user_service.service.CustomOAuth2UserService;
@@ -16,21 +18,21 @@ import lombok.RequiredArgsConstructor;
 public class SecurityConfig {
 	
 	private final CustomOAuth2UserService customOAuth2UserService;
-	
+
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
-			.csrf(csrf -> csrf.disable())
-			.headers(header -> header.frameOptions(frameOption -> frameOption.disable()))
-			.authorizeHttpRequests(auth -> auth
-					.requestMatchers("/", "/login/google").permitAll()
-					.anyRequest().authenticated()
-			)
-			.logout(logout -> logout.logoutSuccessUrl("/"))
-			.oauth2Login(oauth2 -> oauth2
-					.loginPage("/login/google")
-					.userInfoEndpoint(endPoint -> endPoint.userService(customOAuth2UserService))
-					.defaultSuccessUrl("/", true));
+				.csrf(AbstractHttpConfigurer::disable)  // CSRF 비활성화
+				.headers(header -> header.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))  // 헤더 설정
+				.authorizeHttpRequests(auth -> auth
+						.requestMatchers("/", "/login/google").permitAll()  // 메인 페이지와 구글 로그인 링크는 허용
+						.anyRequest().authenticated()  // 그 외 경로는 인증 필요
+				)
+				.logout(logout -> logout.logoutSuccessUrl("/"))  // 로그아웃 후 리다이렉트
+				.oauth2Login(oauth2 -> oauth2
+						.userInfoEndpoint(endPoint -> endPoint.userService(customOAuth2UserService))  // 커스텀 OAuth2 서비스
+						.defaultSuccessUrl("/", true)  // 로그인 성공 후 메인 페이지로 리다이렉트
+				);
 		return http.build();
 	}
 
--- src/main/java/com/vaultis/vaultis_user_service/controller/UserController.java
+++ src/main/java/com/vaultis/vaultis_user_service/controller/UserController.java
@@ -4,16 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class Test {
+public class UserController {
 
     @GetMapping("/")
-    public String home() {
-        return "home"; // home.html을 반환
+    public String mainPage() {
+        return "login";
     }
 
     @GetMapping("/login/google")
     public String login() {
-        // "/oauth2/authorization/google"로 리디렉션하여 구글 로그인 페이지로 이동
         return "redirect:/oauth2/authorization/google";
     }
+
 }
